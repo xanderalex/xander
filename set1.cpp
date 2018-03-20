0dff8ca572f7602051398836a0dea44412c27dbd
xanderalex <37579869+xanderalex@users.noreply.github.com>
2018-03-20 16:56:19 +0530

    Create set1.cpp

---
+++ set1.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the no.");
+scanf("%d",&a);
+if(a==0)
+{
+printf("given value is zero");
+}
+elseif(a>=1)
+{
+printf(" given value is positive");
+}
+else
+{
+printf("given value is negative")
+}
+}
